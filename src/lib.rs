32bf2d36efa204331f9e7db45b08792ae0a770c3
Bernhard Schuster <bernhard@ahoi.io>
2023-12-14 16:48:53 +0100

    initial

---
+++ src/lib.rs
@@ -0,0 +1,204 @@
+use concourse_resource::{InOutput, OutOutput, Resource};
+
+use notion::{
+    ids::DatabaseId,
+    models::{
+        search::{DatabaseQuery, FilterProperty, FilterValue, NotionSearch},
+        Database, DateTime, Page, PageCreateRequest, Parent, Properties, Utc,
+    },
+    NotionApi,
+};
+use reqwest::{Method, Url};
+
+use color_eyre::eyre::*;
+use serde::{Deserialize, Serialize};
+use std::future::Future;
+
+// use
+use fs_err as fs;
+
+#[derive(Deserialize, Serialize, PartialEq, Eq, Clone)]
+pub struct Version {
+    id: DatabaseId,
+    last_edited_time: DateTime<Utc>,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct SourceConfig {
+    /// The API token for accessing the notion database.
+    api_token: String,
+    /// The database name to modify.
+    database: String,
+}
+
+async fn notion_api_client(config: &SourceConfig) -> notion::NotionApi {
+    notion::NotionApi::new(config.api_token.clone()).expect("Creating notion API client works. qed")
+}
+
+async fn lookup_db(notion: &NotionApi, config: &SourceConfig) -> Result<Database> {
+    let objects = notion
+        .search(NotionSearch::Filter {
+            property: FilterProperty::Object,
+            value: FilterValue::Database,
+        })
+        .await?;
+    let mut dbs = Vec::from_iter(
+        objects
+            .only_databases()
+            .results
+            .into_iter()
+            .filter(|x| x.title_plain_text() == config.database),
+    );
+    let db = match dbs.len() {
+        0 => bail!("Counldn't find that DB: {}", config.database),
+        1 => dbs
+            .pop()
+            .expect("We have one item, hence pop() always works. qed"),
+        n => bail!("Ambiguous db name, found {n} matches."),
+    };
+    Ok(db)
+}
+
+async fn check(config: SourceConfig) -> Result<Version> {
+    let notion = notion_api_client(&config).await;
+    let db = lookup_db(&notion, &config).await?;
+    Ok(Version {
+        id: db.id,
+        last_edited_time: db.last_edited_time,
+    })
+}
+
+async fn get(config: SourceConfig, at: Version) -> Result<Vec<Page>> {
+    let notion = notion_api_client(&config).await;
+    let db = lookup_db(&notion, &config).await?;
+    let reality = Version {
+        id: db.id.clone(),
+        last_edited_time: db.last_edited_time,
+    };
+    if at != reality {
+        bail!("Database was modified since checking")
+    }
+    let items = notion
+        .query_database(db.id, DatabaseQuery::default())
+        .await?;
+    Ok(items.results)
+}
+
+#[allow(dead_code)]
+async fn wild<B, R>(api_token: String, _method: Method, path: &str, body: B) -> Result<R>
+where
+    B: Serialize,
+    R: for<'de> Deserialize<'de>,
+{
+    let client = reqwest::Client::new();
+    let req = client
+        .request(
+            Method::POST,
+            Url::parse(&("https://api.notion.com".to_owned() + path)).unwrap(),
+        )
+        .bearer_auth(api_token)
+        .header("Notion-Version", "2022-06-28")
+        .json(&body)
+        .build()?;
+
+    let resp = client.execute(req).await?;
+    if resp.status().as_u16() != 200 {
+        bail!(
+            "Failed submission with response code {}",
+            resp.status().as_u16()
+        );
+    } else {
+        let resp: R = resp.json().await?;
+        Ok(resp)
+    }
+}
+
+async fn put(config: SourceConfig, inject: Vec<Properties>) -> Result<Version> {
+    let notion = notion_api_client(&config).await;
+    let db = lookup_db(&notion, &config).await?;
+
+    assert_eq!("1f0b1e3774d542ae89a95954b646616d", db.id.to_string());
+
+    if inject.is_empty() {
+        bail!("Nothing to update");
+    }
+    let mut version = Version {
+        id: db.id.clone(),
+        last_edited_time: db.last_edited_time,
+    };
+    for extra in inject {
+        let page_create_req = PageCreateRequest {
+            parent: Parent::Database {
+                database_id: db.id.clone(),
+            },
+            properties: extra,
+        };
+        let page = notion.create_page(page_create_req).await?;
+        version.last_edited_time = page.last_edited_time;
+    }
+
+    Ok(version)
+}
+
+fn run_this<T>(future: impl Future<Output = Result<T>>) -> Result<T> {
+    let rt = tokio::runtime::Runtime::new()?;
+    let output = rt.block_on(future)?;
+    Ok(output)
+}
+
+#[derive(Debug, Clone)]
+pub struct NotionResource;
+
+impl Resource for NotionResource {
+    type Version = Version;
+    type Source = SourceConfig;
+    type InParams = concourse_resource::Empty;
+    type InMetadata = concourse_resource::Empty;
+    type OutParams = concourse_resource::Empty;
+    type OutMetadata = concourse_resource::Empty;
+
+    fn resource_check(
+        source: Option<Self::Source>,
+        _version: Option<Self::Version>,
+    ) -> Vec<Self::Version> {
+        let source = source.expect("Must provide `source:` values in resource configuration");
+        let version = run_this(check(source)).expect("Version query should succeed");
+        vec![version]
+    }
+
+    fn resource_in(
+        source: Option<Self::Source>,
+        version: Self::Version,
+        _params: Option<Self::InParams>,
+        output_path: &str,
+    ) -> Result<InOutput<Self::Version, Self::InMetadata>, Box<dyn std::error::Error>> {
+        let source = source.expect("Must provide `source:` values in resource configuration");
+        let items = run_this(get(source, version.clone()))?;
+        let s = serde_json::to_string(&items)?;
+        fs::write(output_path, s.as_bytes())?;
+        std::result::Result::Ok::<_, Box<dyn std::error::Error + 'static>>(InOutput {
+            version,
+            metadata: None,
+        })
+    }
+
+    fn resource_out(
+        source: Option<Self::Source>,
+        _params: Option<Self::OutParams>,
+        input_path: &str,
+    ) -> OutOutput<Self::Version, Self::OutMetadata> {
+        let source = source.expect("Must provide `source:` values in resource configuration");
+        let items = fs::OpenOptions::new()
+            .truncate(true)
+            .create(true)
+            .read(true)
+            .open(input_path)
+            .unwrap();
+        let items: Vec<Properties> = serde_json::from_reader(items).unwrap();
+        let new_version = run_this(put(source, items)).expect("Shall never fail. qed");
+        OutOutput {
+            version: new_version,
+            metadata: None,
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,4 @@
+use concourse_resource::*;
+use concourse_resource_notion_db::NotionResource;
+
+concourse_resource::create_resource!(NotionResource);
